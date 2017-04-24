60ed1443fc5511e51ab380f136d95fc39ee35bee
stites <sam@stites.io>
2017-04-24 02:20:34 -0400

    wip - render cartpole

--- src/Environments/CartPole.hs
+++ src/Environments/CartPole.hs
@@ -23,6 +23,7 @@ import Control.MonadMWCRandom
 import Data.DList
 import Data.Maybe
 import qualified System.Random.MWC as MWC
+import SDL.Video
 
 import Reinforce.Prelude
 import Data.CartPole
@@ -184,13 +185,60 @@ instance MonadEnv Environment StateCP Action Reward where
         "You are calling step even though this environment has already returned done = True."
         ++ "You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior."
 
-  -- | no reward function is needed when interacting with the OpenAI gym
-  -- reward :: Action -> Environment Reward
-  -- reward _ = return 0
+RenderModes = Human | RGBArray
+videoFPS = 50
 
-  -- | no action needs to be run when interacting with the OpenAI gym
-  -- runAction :: Action -> Environment ()
-  -- runAction _ = return ()
+_render :: RenderModes -> Maybe Viewer -> Bool -> Int -> m (Maybe Viewer)
+_render m v close fps = do
+  if close && isJust v
+  then closeViewer (fromJust v) >> return (Nothing)
+  else if (isNothing v)
+    then
+      -- from gym.envs.classic_control import rendering
+      -- self.viewer = rendering.Viewer(screen_width, screen_height)
+      -- l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
+      -- axleoffset =cartheight/4.0
+      -- cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+      -- self.carttrans = rendering.Transform()
+      -- cart.add_attr(self.carttrans)
+      -- self.viewer.add_geom(cart)
+      -- l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
+      -- pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+      -- pole.set_color(.8,.6,.4)
+      -- self.poletrans = rendering.Transform(translation=(0, axleoffset))
+      -- pole.add_attr(self.poletrans)
+      -- pole.add_attr(self.carttrans)
+      -- self.viewer.add_geom(pole)
+      -- self.axle = rendering.make_circle(polewidth/2)
+      -- self.axle.add_attr(self.poletrans)
+      -- self.axle.add_attr(self.carttrans)
+      -- self.axle.set_color(.5,.5,.8)
+      -- self.viewer.add_geom(self.axle)
+      -- self.track = rendering.Line((0,carty), (screen_width,carty))
+      -- self.track.set_color(0,0,0)
+      -- self.viewer.add_geom(self.track)
+      -- if self.state is None: return None
+      undefined
+    else
+      undefined
+
+--         x = self.state
+--         cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
+--         self.carttrans.set_translation(cartx, carty)
+--         self.poletrans.set_rotation(-x[2])
+
+--         return self.viewer.render(return_rgb_array = mode=='rgb_array')
+  where
+    screen_width = 600
+    screen_height = 400
+
+    world_width = x_threshold * 2
+    scale = screen_width / world_width
+    carty = 100 -- TOP OF CART
+    polewidth = 10.0
+    polelen = scale * 1.0
+    cartwidth = 50.0
+    cartheight = 30.0
 
 
 
