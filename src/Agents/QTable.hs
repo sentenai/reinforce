61aa6d256ea5b1ac8656a68aef1fa46b95ae7ac7
stites <sam@stites.io>
2017-04-25 18:22:47 -0400

    use hashmap instead of matrix

--- src/Agents/QTable.hs
+++ src/Agents/QTable.hs
@@ -1,40 +1,17 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 module Agents.QTable where
 
 import Agents.Prelude
+import Agents
 import Environments.Gym.FrozenLakeV0
 import Control.MonadEnv.Internal as Env
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
 import Control.MonadMWCRandom
 
 import Numeric.LinearAlgebra.Static
-
-
--- Just a writer alias
-class (Monoid a, Monad m) => History m a where
-  record   :: a -> m ()
-  tabluate :: m x -> a
-
-
-data Configs = Configs
-  { lambda   :: Double
-  , gamma    :: Double
-  , epsilon  :: Double
-  , maxSteps :: Maybe Int
-  }
-
-defaultConfigs :: Configs
-defaultConfigs = Configs 0.85 0.99 0.1 (Just 2000)
-
-
-data QTableState = QTableState
-  { qs :: L 4 4
-  }
-
-defaultQTableState :: QTableState
-defaultQTableState = QTableState $ matrix $ replicate (4*4) 0
 
 
 class Monad m => QLearning m s a | m -> s a where
@@ -43,23 +20,6 @@ class Monad m => QLearning m s a | m -> s a where
   calcQ   :: s -> a -> m Reward
   update  :: s -> a -> Reward -> s -> m ()
 
-
-newtype QTable x = QTable
-  { getQTable :: RWST Configs [Event] QTableState Environment x }
-  deriving
-    ( Functor
-    , Applicative
-    , Monad
-    , MonadIO
-    , MonadReader Configs
-
-    )
-instance MonadMWCRandom QTable where
-  getGen = liftIO getGen
-
-instance MonadEnv QTable StateFL Action Reward where
-  step a = QTable $ lift (step a)
-  reset = QTable $ lift reset
 
 rolloutQLearning
   :: Maybe Integer
@@ -78,10 +38,56 @@ rolloutQLearning maxSteps = do
           update s a r s'
           clock maxSteps (st+1) (goM s')
 
+
+data Configs = Configs
+  { lambda   :: Double
+  , gamma    :: Double
+  , epsilon  :: Double
+  , maxSteps :: Maybe Int
+  , initialQ :: Reward
+  }
+
+defaultConfigs :: Configs
+defaultConfigs = Configs 0.85 0.99 0.1 (Just 2000) 0
+
+
+data QTableState = QTableState
+  { qs :: HashMap StateFL (HashMap Action Reward)
+  }
+
+qsL :: Lens' QTableState (HashMap StateFL (HashMap Action Reward))
+qsL = lens qs $ \(QTableState _) a -> QTableState a
+
+defaultQTableState :: QTableState
+defaultQTableState = QTableState $ mempty
+
+
+newtype QTable x = QTable
+  { getQTable :: RWST Configs [Event] QTableState Environment x }
+  deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadIO
+    , MonadReader Configs
+    , MonadWriter [Event]
+    , MonadState QTableState
+    , MonadRWS Configs [Event] QTableState
+    )
+
+instance MonadMWCRandom QTable where
+  getGen = liftIO getGen
+
+instance MonadEnv QTable StateFL Action Reward where
+  step a = QTable $ lift (step a)
+  reset = QTable $ lift reset
+
 instance QLearning QTable StateFL Action where
   choose :: StateFL -> QTable Action
   choose s = do
-    Configs{epsilon} <- ask
+    Configs{epsilon, initialQ} <- ask
+    QTableState{qs} <- get
+    qsL %= HM.alter (addNew initialQ) s
     p <- uniform
     as <- actions s
 
@@ -90,6 +96,13 @@ instance QLearning QTable StateFL Action where
     else do
       i <- uniformR (0, length as)
       return . unsafeHead $ drop (i-1) as
+    where
+      addNew :: Reward -> Maybe (HashMap Action Reward) -> Maybe (HashMap Action Reward)
+      addNew x    Nothing = Just . initalTable $ x
+      addNew _ x@(Just _) = x
+
+      initalTable :: Reward -> HashMap Action Reward
+      initalTable x = HM.fromList $ zip [minBound..maxBound::Action] [x..]
 
 
   actions :: StateFL -> QTable [Action]
@@ -107,45 +120,5 @@ instance QLearning QTable StateFL Action where
     newQs <- sequence . map (calcQ s') =<< actions s'
     let updQ = oldQ + lambda * (r + gamma * (maximum newQs) - oldQ)
     return ()
-
-
--- for i in range(num_episodes):
---     #Reset environment and get first new observation
---     s = env.reset()
---     rAll = 0
---     d = False
---     j = 0
---     #The Q-Table learning algorithm
---     while j < 99:
---         j+=1
---         #Choose an action by greedily (with noise) picking from Q table
---         a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
---         #Get new state and reward from environment
---         s1,r,d,_ = env.step(a)
---         #Update Q-Table with new knowledge
---         Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
---         rAll += r
---         s = s1
---         if d == True:
---             break
---     #jList.append(j)
---     rList.append(rAll)
-
-runLearner
-  :: forall m o a r . MonadEnv m o a r
-  => Maybe Integer
-  -> Maybe Integer
-  -> (Maybe Integer -> m ())
-  -> m ()
-runLearner maxEpisodes maxSteps rollout = clock maxEpisodes 0 goM
-  where
-   goM :: Integer -> m ()
-   goM epn =
-     rollout maxSteps
-     >> clock maxEpisodes (epn + 1) goM
-
-clock :: Monad m => Maybe Integer -> Integer -> (Integer -> m ()) -> m ()
-clock   Nothing n goM = goM n
-clock (Just mx) n goM = unless (n >= mx) (goM n)
 
 
---
+++ src/Internal/MonadHistory.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Internal.MonadHistory where
+
+import Agents.Prelude
+
+-- Just a writer alias for the time being... which probably means I should replace it with Writer...
+class (Monoid h, Monad m) => MonadHistory h m | m -> h where
+  {-# MINIMAL (history | record), observe, specify #-}
+  history :: (a, h) -> m a
+  history (a, h) = record h >> return a
+
+  record :: h -> m ()
+  record h = history (() , h)
+
+  observe :: m a -> m (a, w)
+
+  specify :: m (a, w -> w) -> m a
+
+-- instance (Monoid w, MonadWriter w m) => MonadHistory w (Writer.WriterT w m) where
+--   history = Writer.writer
+--   record  = Writer.tell
+--   observe = Writer.listen
+--   specify = Writer.pass
+
+
+-- instance (Monoid w, Monad m) => MonadHistory w (RWST r w s m) where
+--   history = RWS.writer
+--   record  = RWS.tell
+--   observe = RWS.listen
+--   specify = RWS.pass
+
+
