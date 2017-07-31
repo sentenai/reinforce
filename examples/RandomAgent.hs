0a663ff8be21ee0d8d14417182223049cd531d2d
stites <sam@stites.io>
2017-07-31 14:37:46 -0400

    bump example and copy to readme

--- examples/RandomAgent.hs
+++ examples/RandomAgent.hs
@@ -1,32 +1,44 @@
 module Main where
 
 import Reinforce.Prelude
-import Control.MonadEnv as Env
-import Environments.CartPole
-import Spaces.Action (randomChoice)
+    -- ^ NoImplicitPrelude is on
+
+import Environments.CartPole (Environment, runEnvironment_)
+import Control.MonadEnv      (Initial(..), Obs(..))
+
+import qualified Control.MonadEnv       as Env (step, reset)
+import qualified Environments.CartPole  as Env (StateCP, Action)
+    -- Comments:
+    --     StateCP - An "observation" or "the state of the agent" - note that State overloaded, so StateCP
+    --     Action  - A performable action in the environment.
+import qualified Spaces.Action          as Actions (randomChoice)
 
 main :: IO ()
-main = runEnvironment_ $
-  forM_ [0..maxEpisodes] $ \_ ->
-    Env.reset >>= \case
-      EmptyEpisode -> pure()
-      Initial obs  -> do
-        liftIO . print $ "Initialized episode and am in state " ++ show obs
-        rolloutEpisode obs
+main = runEnvironment_ gogoRandomAgent
 
   where
+    gogoRandomAgent :: Environment ()
+    gogoRandomAgent = forM_ [0..maxEpisodes] $ \_ ->
+      Env.reset >>= \case           -- this comes from LambdaCase. Sugar for: \a -> case a of ...
+        EmptyEpisode -> pure ()
+        Initial obs  -> do
+          liftIO . print $ "Initialized episode and am in state " ++ show obs
+          rolloutEpisode obs 0
+
     maxEpisodes :: Int
     maxEpisodes = 100
 
-    rolloutEpisode :: StateCP -> Environment ()
-    rolloutEpisode obs = do
-      a <- liftIO randomChoice
+    -- this is usually the structure of a rollout:
+    rolloutEpisode :: Env.StateCP -> Double -> Environment ()
+    rolloutEpisode obs totalRwd = do
+      a <- liftIO Actions.randomChoice
       Env.step a >>= \case
         Done r mobs  ->
           liftIO . print
-            $ "Done! final reward: " ++ show r ++ ", final state: " ++ show mobs
+            $ "Done! final reward: " ++ show (totalRwd+r) ++ ", final state: " ++ show mobs
         Next r  obs' -> do
           liftIO . print
-            $ "Stepped with " ++ show a ++ ". reward: " ++ show r ++ ", next state: " ++ show obs'
-          rolloutEpisode obs'
+            $ "Stepped with " ++ show a ++ " - reward: " ++ show r ++ ", next state: " ++ show obs'
+          rolloutEpisode obs' (totalRwd+r)
+
 
